f4b9fffebcfdb0befb22611af8ea331ea5aa3990
Kamran Safi <64833377+kamransafi@users.noreply.github.com>
2021-02-23 15:01:03 +0100

    Delete ThinNSave.r

--- ThinNSave.r
+++
@@ -1,6 +0,0 @@
-simpleThin <- function(x){
-  eagle <- getMovebankData(study, creds, animal=x, removeDuplicatedTimestamps=T)
-  eagle1h <- eagle[!duplicated(cbind(year(timestamps(eagle)), month(timestamps(eagle)), day(timestamps(eagle)), hour(timestamps(eagle)))),]
-  message(paste("Done with ", x, ".", sep=""))
-  return(eagle1h)
-}
