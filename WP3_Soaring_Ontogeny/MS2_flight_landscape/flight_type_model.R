6eaf88057327a90600324bdc54b2e3104a29b86f
mahle68 <mahle68@gmail.com>
2022-05-09 14:19:26 +0200

    tried with low res terrain. no change

--- WP3_Soaring_Ontogeny/MS2_flight_landscape/flight_type_model.R
+++ WP3_Soaring_Ontogeny/MS2_flight_landscape/flight_type_model.R
@@ -13,10 +13,15 @@ library(mapview)
 #build a binomial logistic regression, with thermal soaring as 0 and slope soaring as 1
 #or, even better, make a multinomial model
 
-#open terrain layers and put onto one stack
-ls <- list.files("/home/enourani/Desktop/golden_eagle_static_layers", pattern = "100.tif", full.names = T)
-stck <- lapply(ls, rast) %>% 
+#open terrain layers and put onto one stack. there was no difference between flight types regarding terrain at 100 meters. try the higher res
+ls <- list.files("/home/enourani/Desktop/golden_eagle_static_layers", pattern = ".tif", full.names = T)
+High_res <- ls[grep(ls, pattern = "100", invert = T)]
+
+dem <- rast("/home/enourani/ownCloud/Work/GIS_files/EU_DEM/eu_dem_v11_E40N20/eu_dem_vdem_10011_E40N20.TIF")
+
+stck <- lapply(High_res, rast) %>% 
   rast()
+
 
 #open matched data with wind speed and flight type: one individual first
 load("/home/enourani/Desktop/Golden_Eagle_data/gps_acc_age/Almen19 (eobs 7001)_gps_acc_age.RData") #gaa_df 
@@ -29,37 +34,43 @@ one_ind <- gaa_df %>%
 #extract values for tracking points
 tr_ann <- cbind(one_ind, extract(x = stck, y = one_ind, method = "bilinear"))
 
-saveRDS(tr_ann, file = "one_ind_terrain_annotation.rds") #raster file
+saveRDS(tr_ann, file = "one_ind_terrain_annotation.rds") #raster file (100m res)
 
 tr_df <- tr_ann %>%
   project("+proj=longlat +datum=WGS84") %>% #project back to wgs 
   as.data.frame(geom = "XY") #keep coordinates in the df
 
-saveRDS(tr_df, file = "one_ind_terrain_annotation_df.rds") #df file
+saveRDS(tr_df, file = "one_ind_terrain_annotation_df.rds") #df file (100m res)
+saveRDS(tr_df, file = "one_ind_terrain_annotation_df_hres.rds") #df file (original res)
 
 # ----------- STEP 2: cluster/PCA to classify flight type based on the terrain -----------------
 
 #make density plots comparing the two categories: all look very similar
 
-ggplot(tr_df, aes(x = TRI_100)) +
+ggplot(tr_df, aes(x = TRI)) +
   geom_density(aes(color = thermalClust)) +
-  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))
+  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))+
+  facet_wrap(~stage)
   
-ggplot(tr_df, aes(x = dem_100)) +
+ggplot(tr_df, aes(x = dem)) +
   geom_density(aes(color = thermalClust)) +
-  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))
+  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))+
+  facet_wrap(~stage)
 
-ggplot(tr_df, aes(x = slope_TPI_100)) +
+ggplot(tr_df, aes(x = slope_TPI)) +
   geom_density(aes(color = thermalClust)) +
-  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))
+  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))+
+  facet_wrap(~stage)
 
-ggplot(tr_df, aes(x = aspect_TPI_100)) +
+ggplot(tr_df, aes(x = aspect_TPI)) +
   geom_density(aes(color = thermalClust)) +
-  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))
+  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))+
+  facet_wrap(~stage)
 
-ggplot(tr_df, aes(x = slope_100)) +
+ggplot(tr_df, aes(x = slope)) +
   geom_density(aes(color = thermalClust)) +
-  scale_color_manual(values = c("gray48", "gold2", "forestgreen"))
+  scale_color_manual(values = c("gray48", "gold2", "forestgreen")) +
+  facet_wrap(~stage)
 
 
 #check for correlation
