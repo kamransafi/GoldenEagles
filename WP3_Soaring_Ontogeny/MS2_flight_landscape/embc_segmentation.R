f25963bc238bcc06dd5d7ad8ca002d655c93a68f
mahle68 <mahle68@gmail.com>
2022-06-16 09:53:37 +0200

    updated for 70 individuals

--- WP3_Soaring_Ontogeny/MS2_flight_landscape/embc_segmentation.R
+++ WP3_Soaring_Ontogeny/MS2_flight_landscape/embc_segmentation.R
@@ -1,6 +1,7 @@
 #script for analysis of golden eagle data for the dynamics of the energy landscape manuscript: data clean up and segmentation
 #full data set downloaded from Movebank on Jan. 13. 2022
 #Jan 11. 2022. Elham Nourani. Konstanz, DE
+#Update June 15. 20222: use 70 individuals (emigration timing was estimated using the recurse package)
 
 library(tidyverse)
 library(lubridate)
@@ -8,6 +9,7 @@ library(move)
 library(sf)
 library(EMbC)
 library(mapview)
+library(terra)
 
 wgs <- CRS("+proj=longlat +datum=WGS84 +no_defs")
 meters_proj <- CRS("+proj=moll +ellps=WGS84")
@@ -23,100 +25,100 @@ getmode <- function(v) {
 
 #open file with all data
 data <- read.csv("/home/enourani/Desktop/Golden_Eagle_data/all_GPS_jan13_22/LifeTrack Golden Eagle Alps.csv", encoding = "UTF-8") %>% 
-  mutate(id = strsplit(individual.local.identifier, " ") %>% map_chr(., 1),
-         timestamp = as.POSIXct(strptime(timestamp,format = "%Y-%m-%d %H:%M:%S"),tz = "UTC"))
+         mutate(timestamp = as.POSIXct(strptime(timestamp, format = "%Y-%m-%d %H:%M:%S"),tz = "UTC"))
+
+#15 min subset (get rid of super bursts)
+data_15 <- data %>% 
+    mutate(dt_15min = round_date(timestamp, "15 minutes")) %>% 
+    group_by(individual.local.identifier,dt_15min) %>% 
+    slice(1) %>% 
+    drop_na(location.long) %>% 
+    as.data.frame()
 
 
-#n_distinct(data$id) is 78
+saveRDS(data_15, file = "/home/enourani/Desktop/Golden_Eagle_data/all_GPS_jan13_22/LifeTrack Golden Eagle Alps_15min.rds")
 
-
-## Hester's files on matching the names
-load("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/data/from_Hester/eagle_names.RData") #eagle_names
-
-#open file with info on fledging and emigration timing (from Svea)
-dates <- read.csv("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/data/Goldeneagles_emigration_time_10_2021.csv",
-                  stringsAsFactors = F, fileEncoding = "latin1") %>%  
-  rowwise() %>% 
-  mutate(fledging_timestamp = paste(paste(strsplit(date_fledging, "\\.") %>% map_chr(., 3), #yr 
-                                          strsplit(date_fledging, "\\.") %>% map_chr(., 2), #mnth
-                                          strsplit(date_fledging, "\\.") %>% map_chr(., 1), sep = "-"),  #day
-                                    time_fledging, sep = " "),
-         emigration_timestamp = ifelse(is.na(date_emigration), NA , 
-                                       paste(paste(strsplit(date_emigration, "\\.") %>% map_chr(., 3), #yr 
-                                                   strsplit(date_emigration, "\\.") %>% map_chr(., 2), #mnth
-                                                   strsplit(date_emigration, "\\.") %>% map_chr(., 1), sep = "-"),  #day
-                                             time_emigration, sep = " "))) %>% 
-  ungroup() %>% 
-  mutate(fledging_timestamp = as.POSIXct(strptime(fledging_timestamp,format = "%Y-%m-%d %H:%M:%S"),tz = "UTC"),
-         emigration_timestamp = as.POSIXct(strptime(emigration_timestamp,format = "%Y-%m-%d %H:%M:%S"),tz = "UTC")) %>% 
-  full_join(eagle_names, by = c("id" = "age_name")) %>% 
-  as.data.frame()
-
-
-#n_distinct(dates$id) is 32
+#open file containing emigration dates
+load("/home/enourani/Desktop/Hester_GE/recurse/em_fl_dt_recurse_70ind.RData") #emig_fledg_dates
           
 #only keep individuals that we have emigration info
-emig_dates <- dates %>% 
-  drop_na(emigration_timestamp)
+data_w_info <- data_15 %>% 
+  filter(individual.local.identifier %in% emig_fledg_dates$individual.local.identifier) #n = 70
 
-#n_distinct(emig_dates$id) is 25
-
-data_w_info <- data %>% 
-  filter(data$individual.local.identifier %in% emig_dates$local_identifier) #n = 25
-
-save(data_w_info, file = "all_data_w_emig.RData")
-
+saveRDS(data_w_info, file = "/home/enourani/Desktop/Golden_Eagle_data/all_GPS_jan13_22/LifeTrack Golden Eagle Alps_15min_w_info.rds")
 
 # STEP 2: assign life stages: filter post-emigration ----------------------------------------------------------------
 
 ind_ls <- split(data_w_info, data_w_info$individual.local.identifier)
 
 data_stage <- lapply(ind_ls, function(x){
-  d <- emig_dates %>% 
-    filter(local_identifier ==  unique(x$individual.local.identifier))
+  d <- emig_fledg_dates %>% 
+    filter(individual.local.identifier ==  unique(x$individual.local.identifier))
   
   x <- x %>% 
-    mutate(stage = ifelse(timestamp < d$fledging_timestamp, "pre-fledging",
+    mutate(stage = ifelse(timestamp < d$fledging_dt, "pre-fledging",
                           ifelse(
-                            between(timestamp, d$fledging_timestamp, d$emigration_timestamp), "post_fledging", #this category includes the first day of fleding
+                            between(timestamp, d$fledging_dt, d$emigration_dt), "post_fledging", #this category includes the first day of fleding
                             ifelse(
-                              timestamp >= d$emigration_timestamp,"post_emigration", NA))))
+                              timestamp >= d$emigration_dt,"post_emigration", NA))))
   
   x
 }) %>% 
   reduce(rbind)
 
-save(data_stage, file = "data_w_lifestage.RData")
+save(data_stage, file = "data_w_lifestage_15min_70.RData")
 
 #extract post-emigration data
 post_em <- data_stage %>% 
   filter(stage == "post_emigration")
 
-save(post_em, file = "post_em_df.RData")
+save(post_em, file = "post_em_df_15min_70.RData")
 
 
 # STEP 3: estimate flight height ----------------------------------------------------------------
 
-load( "post_em_df.RData")
+load( "post_em_df_15min_70.RData") #post_em
+
+#prepare DEM (merge east and west Alps)
+
+dem <- rast("/home/enourani/Desktop/golden_eagle_static_layers/French_Alps/dem_100.tif")
+dem2 <- rast("/home/enourani/Desktop/golden_eagle_static_layers/dem_100.tif")
+
+Alps <- st_read("/home/enourani/ownCloud/Work/GIS_files/Alpine_perimeter/Alpine_Convention_Perimeter_2018_v2.shp") %>% 
+  st_transform(crs(dem)) %>% 
+  as("SpatVector")
+  
+dem_east <- mask(dem2,Alps)
+dem_west <- mask(dem, Alps)
+
+Alps_topo <- merge(dem_east,dem_west)
+
+writeRaster(Alps_topo, "Alps_east_west_dem.tif")
+  
+#reproject
+alps_topo_wgs <- Alps_topo %>% 
+  project("+proj=longlat +datum=WGS84 +no_defs")
 
 #open EU-DEM
-dem <- raster("/home/enourani/ownCloud/Work/GIS_files/EU_DEM/eu_dem_v11_E40N20/eu_dem_v11_E40N20.TIF")
+#dem <- raster("/home/enourani/ownCloud/Work/GIS_files/EU_DEM/eu_dem_v11_E40N20/eu_dem_v11_E40N20.TIF")
 
-dem_wgs <- projectRaster(dem, crs = wgs) 
+#dem_wgs <- projectRaster(dem, crs = wgs) 
 
-save(dem_wgs, file = "/home/enourani/ownCloud/Work/GIS_files/EU_DEM/eu_dem_v11_E40N20/dem_wgs.RData")
+#save(dem_wgs, file = "/home/enourani/ownCloud/Work/GIS_files/EU_DEM/eu_dem_v11_E40N20/dem_wgs.RData")
 
 #extract elevation values
-post_em$dem_alt <- extract(x = dem_wgs, y = post_em[,c("location.long","location.lat")], method = "bilinear")
+#load("/home/enourani/ownCloud/Work/GIS_files/EU_DEM/eu_dem_v11_E40N20/dem_wgs.RData")
+post_em$dem_alt <- extract(x = alps_topo_wgs, y = post_em[,c("location.long","location.lat")], method = "bilinear")[,2] 
+
 
 #calculate flight height as ellipsoid-dem
 post_em$flight_h <- post_em$height.above.ellipsoid - post_em$dem_alt
 
-save(post_em, file = "post_em_df_dem.RData")
+save(post_em, file = "post_em_df_dem_70ind.RData")
 
 # STEP 4: subset to 1 min intervals and estimate ground speed ----------------------------------------------------------------
 
-load("post_em_df_dem.RData") #post_em
+load("post_em_df_dem_70ind.RData") #post_em
 
 #remove duplicated timestamps
 rows_to_delete <- unlist(sapply(getDuplicatedTimestamps(x = post_em$individual.local.identifier, timestamps = post_em$timestamp,
@@ -125,36 +127,24 @@ rows_to_delete <- unlist(sapply(getDuplicatedTimestamps(x = post_em$individual.l
 
 post_em <- post_em[-rows_to_delete,] 
 
-post_em <- post_em[order(c(post_em$individual.local.identifier, post_em$timestamp)),]
+post_em <- post_em %>% 
+  arrange(individual.local.identifier,timestamp) %>% 
+  as.data.frame()
 
 #convert to a move object.
 mv <- move(x = post_em$location.long, y = post_em$location.lat, time = post_em$timestamp, proj = wgs, data = post_em, animal = post_em$individual.local.identifier)
 mv$speed <- unlist(lapply(speed(mv),c, NA ))
 
-save(mv, file = "post_em_mv.RData") #move object with original data frequency and NA values
+save(mv, file = "post_em_mv_70_15min.RData") #move object with 15 min frequency and NA values
 
 #remove NA values of flight_h
 mv_no_na <- mv[!is.na(mv$flight_h),]
 
-#keep the first instance of every minute.
-df_mnt <- as.data.frame(mv_no_na) %>%
-  group_by(individual.local.identifier, year(timestamp), month(timestamp), day(timestamp), hour(timestamp), minute(timestamp)) %>%
-  slice(1) %>% 
-  dplyr::select(!c("coords.x2" ,"coords.x1", "year(timestamp)","month(timestamp)", "day(timestamp)","hour(timestamp)", "minute(timestamp)")) %>% #remove columns that I don't need
-  as.data.frame()
-
-#convert back to a move object
-mv_mnt <- move(x = df_mnt$location.long, y = df_mnt$location.lat, time = df_mnt$timestamp, proj = wgs, data = df_mnt, animal = df_mnt$individual.local.identifier)
-
 #calculate ground speed and time interval between points
-mv_mnt$speed <- unlist(lapply(speed(mv_mnt),c, NA ))
-mv_mnt$time_lag <- unlist(lapply(timeLag(mv_mnt, units = "mins"),  c, NA))
-
-save(mv_mnt, file = "post_em_mv_minutely.RData") #move object with minutely data frequency and no NA values for flight height
+mv_no_na$speed <- unlist(lapply(speed(mv_no_na),c, NA ))
+mv_no_na$time_lag <- unlist(lapply(timeLag(mv_no_na, units = "mins"),  c, NA))
 
 # STEP 5: EMbC segmentation ----------------------------------------------------------------
-
-load("post_em_mv_minutely.RData") #mv_mnt
 
 #prep for embc (for all individuals pooled. no reliability function will be used for speed, because data collection frequency is pretty uniform)
 
@@ -217,7 +207,7 @@ load("post_em_mv_minutely.RData") #mv_mnt
 
 ###what if i remove the outliers?
 
-input <- as.data.frame(mv_mnt) %>%
+input <- as.data.frame(mv_no_na) %>%
   drop_na(speed) %>% #remove NA values for speed
   filter(between(flight_h, quantile(flight_h, 0.005), quantile(flight_h, 0.999)) & speed < quantile(speed, 0.999)) %>% 
   as.data.frame()
@@ -236,9 +226,9 @@ X11();sctr(bc_smth)
 
 #append cluster labels (1:LL, 2:LH, 3:HL, and4:HH) to original data
 input$embc_clst <- bc@A
-input$embc_clst_smth <- bc_smth2@A
+input$embc_clst_smth <- bc_smth@A
 
-save(input, file = "embc_output.RData")
+saveRDS(input, file = "embc_output_70ind.rds")
 
 #select a sample track and visualize
 smpl <- input %>% 
