4e3d33c390bb66065f62023cf9c1151c3cb9bc02
mahle68 <mahle68@gmail.com>
2022-06-20 14:56:12 +0200

    start of update for 70 ind

--- WP3_Soaring_Ontogeny/MS2_flight_landscape/energy_landscape_modeling_method1.R
+++ WP3_Soaring_Ontogeny/MS2_flight_landscape/energy_landscape_modeling_method1.R
@@ -22,14 +22,12 @@ setwd("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/R_files/")
 wgs <- CRS("+proj=longlat +datum=WGS84 +no_defs")
 
 #open annotated data (static variables and time since fledging and emigration)
-load("alt_50_20_min_25_ind_static_time_ann_weeks.RData") #ann_cmpl
+load( "inla_input_for_annotation_70inds.csv") #ann_cmpl
 
 cmpl_ann <- cmpl_ann %>% 
   mutate(days_since_emig_n = ceiling(as.numeric(days_since_emig)),#round up
-         weeks_since_emig_n = ceiling(as.numeric(weeks_since_emig)), #120 unique weeks
-         stratum = paste(individual.local.identifier, burst_id, step_id, sep = "_"))
+         weeks_since_emig_n = ceiling(as.numeric(weeks_since_emig))) #135 unique weeks
 
- 
 Mode <- function(x, na.rm = FALSE) {
   if(na.rm){
     x = x[!is.na(x)]
@@ -51,81 +49,109 @@ cmpl_ann %>%
 cmpl_ann %>% 
   group_by(individual.local.identifier) %>% 
   summarize(max_day = max(ceiling(as.numeric(days_since_emig)))) %>% 
-  summarize(mode = Mode(max_day), # 537
-            mean = mean(max_day)) #444
+  summarize(mode = Mode(max_day), #  459
+            median = median(max_day), # 438
+            mean = mean(max_day)) #  438
 
 
 #terrain ~ days since emigration ..... no patterns in the plots
 
-plot(cmpl_ann[cmpl_ann$used == 1, c("days_since_emig", "dem_100")])
-plot(cmpl_ann[cmpl_ann$used == 1, c("days_since_emig", "slope_100")])
-plot(cmpl_ann[cmpl_ann$used == 1, c("days_since_emig", "aspect_100")])
-plot(cmpl_ann[cmpl_ann$used == 1, c("days_since_emig", "slope_TPI_100")])
-plot(cmpl_ann[cmpl_ann$used == 1, c("days_since_emig", "TPI_100")])
-plot(cmpl_ann[cmpl_ann$used == 1, c("days_since_emig", "TRI_100")])
-plot(cmpl_ann[cmpl_ann$used == 1, c("days_since_emig", "aspect_TPI_100")])
+plot(cmpl_ann[cmpl_ann$used == 1, c("weeks_since_emig_n", "dem_100")])
+plot(cmpl_ann[cmpl_ann$used == 1, c("weeks_since_emig_n", "slope_100")])
+plot(cmpl_ann[cmpl_ann$used == 1, c("weeks_since_emig_n", "aspect_100")])
+plot(cmpl_ann[cmpl_ann$used == 1, c("weeks_since_emig_n", "slope_TPI_100")])
+plot(cmpl_ann[cmpl_ann$used == 1, c("weeks_since_emig_n", "TPI_100")])
+plot(cmpl_ann[cmpl_ann$used == 1, c("weeks_since_emig_n", "TRI_100")])
+plot(cmpl_ann[cmpl_ann$used == 1, c("weeks_since_emig_n", "aspect_TPI_100")])
 
 
-ggplot(cmpl_ann[cmpl_ann$used == 1,], aes(as.numeric(days_since_emig), aspect_TPI_100)) +
+ggplot(cmpl_ann[cmpl_ann$used == 1,], aes(as.numeric(weeks_since_emig_n), TRI_100)) +
   geom_point() +
   stat_smooth(aes(group = individual.local.identifier), method = "lm") +
   theme_minimal() +
   theme(legend.position = "none")
 
+ggplot(cmpl_ann[cmpl_ann$used == 1,], aes(as.numeric(weeks_since_emig_n), dem_100)) +
+  geom_point() +
+  stat_smooth(aes(group = individual.local.identifier), method = "lm") +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+ggplot(cmpl_ann[cmpl_ann$used == 1,], aes(as.numeric(weeks_since_emig_n), slope_TPI_100)) +
+  geom_point() +
+  stat_smooth(aes(group = individual.local.identifier), method = "lm") +
+  theme_minimal() +
+  theme(legend.position = "none")
 
 # STEP 2: summary plots ----------------------------------------------------------------
 
 #one set of boxplots for select days: 10, 50 , 100, 500
 
 data_int <- cmpl_ann %>%
-  filter(days_since_emig_n %in% c(1, 10, 30 , 100, 300)) %>% 
-  mutate(days_f = as.factor(days_since_emig_n))
+  filter(weeks_since_emig_n %in% c(1, 2, 4, 10, 50, 100)) %>% 
+  mutate(weeks_f = as.factor(weeks_since_emig_n))
 
 
 variables <- c("dem_100", "slope_100", "aspect_100", "TRI_100", "TPI_100", "slope_TPI_100", "aspect_TPI_100" )
 
 X11(width = 6, height = 9)
 
-png("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/paper_prep/initial_figs/box_plots.png", width = 6, height = 9, units = "in", res = 300)
+png("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/paper_prep/initial_figs/box_plots_n48.png", width = 6, height = 9, units = "in", res = 300)
 
 par(mfrow= c(4,2), 
     oma = c(0,0,3,0), 
     las = 1,
     mgp = c(0,1,0))
 for(i in 1:length(variables)){
-  boxplot(data_int[,variables[i]] ~ data_int[,"days_f"], data = data_int, boxfill = NA, border = NA, main = variables[i], xlab = "", ylab = "")
+  boxplot(data_int[,variables[i]] ~ data_int[,"weeks_f"], data = data_int, boxfill = NA, border = NA, main = variables[i], xlab = "", ylab = "")
   if(i == 1){
     legend("topleft", legend = c("used","available"), fill = c(alpha("darkgoldenrod1", 0.9),"gray"), bty = "n", cex = 0.8)
   }
-  boxplot(data_int[data_int$used == 1, variables[i]] ~ data_int[data_int$used == 1,"days_f"], outcol = alpha("black", 0.2),
+  boxplot(data_int[data_int$used == 1, variables[i]] ~ data_int[data_int$used == 1,"weeks_f"], outcol = alpha("black", 0.2),
           yaxt = "n", xaxt = "n", add = T, boxfill = alpha("darkgoldenrod1", 0.9),  lwd = 0.7, outpch = 20, outcex = 0.8,
-          boxwex = 0.25, at = 1:length(unique(data_int[data_int$used == 1, "days_f"])) - 0.15)
-  boxplot(data_int[data_int$used == 0, variables[i]] ~ data_int[data_int$used == 0, "days_f"], outcol = alpha("black", 0.2),
+          boxwex = 0.25, at = 1:length(unique(data_int[data_int$used == 1, "weeks_f"])) - 0.15)
+  boxplot(data_int[data_int$used == 0, variables[i]] ~ data_int[data_int$used == 0, "weeks_f"], outcol = alpha("black", 0.2),
           yaxt = "n", xaxt = "n", add = T, boxfill = "grey", lwd = 0.7, outpch = 20, outcex = 0.8,
-          boxwex = 0.25, at = 1:length(unique(data_int[data_int$used == 1 , "days_f"])) + 0.15)
+          boxwex = 0.25, at = 1:length(unique(data_int[data_int$used == 1 , "weeks_f"])) + 0.15)
   
 }
 dev.off()
 
 
-#also consider making the plots for periods of time and not only one day
-
 # STEP 3: check for collinearity ----------------------------------------------------------------
 
 cmpl_ann %>% 
-  dplyr::select(c("dem_100", "slope_100", "aspect_100", "TRI_100", "TPI_100", "slope_TPI_100", "aspect_TPI_100", "days_since_emig_n")) %>% 
+  dplyr::select(c("dem_100", "slope_100", "aspect_100", "TRI_100", "TPI_100", "slope_TPI_100", "aspect_TPI_100", "weeks_since_emig_n")) %>% 
   correlate() %>% 
-  stretch() %>% 
+  corrr::stretch() %>% 
   filter(abs(r) > 0.6) #slope and TRI are correlated (.98)
 
 # STEP 4: standardize variables ----------------------------------------------------------------
 
 all_data <- cmpl_ann %>% 
-  mutate_at(c("dem_100", "TRI_100", "days_since_emig_n", "weeks_since_emig_n"),
+  mutate_at(c("dem_100", "slope_100", "aspect_100", "TRI_100", "TPI_100", "slope_TPI_100", "aspect_TPI_100", "weeks_since_emig_n"),
             list(z = ~(scale(.)))) 
 
-#see previous versions for exploration using clogit
 # STEP 5: ssf modeling ----------------------------------------------------------------
+
+## mid_step: investigate using clogit
+
+form_all <- used ~ dem_100_z * weeks_since_emig_n_z + slope_100_z * weeks_since_emig_n_z + 
+  aspect_100_z * weeks_since_emig_n_z + TRI_100_z * weeks_since_emig_n_z +
+  TPI_100_z * weeks_since_emig_n_z + slope_TPI_100_z * weeks_since_emig_n_z + 
+  aspect_TPI_100_z * weeks_since_emig_n_z + weeks_since_emig_n_z * weeks_since_emig_n_z + 
+  strata(stratum)
+ssfa <- clogit(form_all, data = all_data)
+summary(ssfa)
+plot_summs(ssfa)
+
+form1a <- used ~ dem_100_z * weeks_since_emig_n + 
+  TRI_100_z * weeks_since_emig_n + 
+  #TPI_100_z * days_since_emig_n_z + 
+  strata(stratum)
+ssf <- clogit(form1a, data = all_data)
+summary(ssf)
+plot_summs(ssf)
 
 # INLA formula using interaction terms for time and predictor variables.
 # control for month of year or temperature....
