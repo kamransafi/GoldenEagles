6787c3d98b68ad4bb2ba3fb19c0a88717d339e46
mahle68 <mahle68@gmail.com>
2023-01-31 08:48:48 +0100

    update

--- WP3_Soaring_Ontogeny/MS2_flight_landscape/02b_temp_download_&_prep.R
+++ WP3_Soaring_Ontogeny/MS2_flight_landscape/02b_temp_download_&_prep.R
@@ -7,12 +7,12 @@
 library(tidyverse)
 library(lubridate)
 library(sf)
+library(terra)
 library(ncdf4)
 library(reticulate)
 
 setwd("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/R_files/")
-wgs <- CRS("+proj=longlat +datum=WGS84 +no_defs")
-
+wgs <- crs("+proj=longlat +datum=WGS84 +no_defs")
 
 # STEP 1: Prep specs for data download -----------------------------------------------------------------------------------------------
 
@@ -102,4 +102,67 @@ data_temp <- lapply(mnth_yr_ls, function(x){
 saveRDS(data_temp, "alt_50_20_min_70_ind_static_time_ann_temp.rds") #data_temp
 
 
-# STEP 4: process netcdf data: landscape maps -----------------------------------------------------------------------------------------------
+# STEP 4: download data: landscape maps -----------------------------------------------------------------------------------------------
+
+#have a look at the distribution of data across months
+cmpl_ann %>% filter(used == 1) %>% mutate(month = month(timestamp)) %>%  pull(month) %>%  hist()
+
+#let's do June vs Oct for 10 years
+years <- as.character(2013:2022)
+mnths <- c("06", "10")
+
+#area is the same as above
+ext <- round(as.numeric(Alps_extent[c(4,1,2,3)]),2)
+
+#prepare the connection
+path <- "/home/enourani/.local/lib/python2.7/site-packages/"
+cdsapi <- import_from_path("cdsapi", path = path)
+
+server = cdsapi$Client()
+
+output_path <- "/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/data/monthly_temp_cds_10yrs/"
+
+
+for(i in years){
+  for(j in mnths){
+    
+    request <- r_to_py(list(
+      product_type = "monthly_averaged_reanalysis",
+      variable = "2m_temperature", 
+      year = i,
+      month = j,
+      time = "00:00",
+      area = ext,  # North, West, South, East.
+      format = "netcdf",
+      dataset_short_name = "reanalysis-era5-land-monthly-means"
+    ))
+    
+    server$retrieve("reanalysis-era5-land-monthly-means",
+                    request,
+                    target = paste0(output_path,"temp_data_", j, "_", i, ".nc")) 
+    
+  }
+  
+}
+
+# STEP 5: process data: landscape maps -----------------------------------------------------------------------------------------------
+
+ncdf_list <- list.files("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/data/monthly_temp_cds_10yrs", full.names = T)
+
+# I need one value per cell. So, open all 10 layer for each month, take the average per cell, then append to the alpine pred dataframe
+
+#open corresponding temp data as a raster
+Jun_r <- ncdf_list[ncdf_list %>% 
+                      str_which("06")] %>% 
+  rast() %>% 
+  mean()
+
+writeRaster(Jun_r, file = "avg_temp_10yr_Jun.tif")
+
+Oct_r <- ncdf_list[ncdf_list %>% 
+                     str_which("10")] %>% 
+  rast() %>% 
+  mean()
+
+writeRaster(Oct_r, file = "avg_temp_10yr_Oct.tif")
+
