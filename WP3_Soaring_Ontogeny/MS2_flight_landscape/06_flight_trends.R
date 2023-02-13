9a8f9735e859c43732683d7f33817ed28fb1ef2f
mahle68 <mahle68@gmail.com>
2023-02-13 09:32:48 +0100

    update

--- WP3_Soaring_Ontogeny/MS2_flight_landscape/06_flight_trends.R
+++ WP3_Soaring_Ontogeny/MS2_flight_landscape/06_flight_trends.R
@@ -16,7 +16,41 @@ flight <- readRDS("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/
 #emigration date info
 load("/home/enourani/ownCloud/Work/Projects/GE_ontogeny_of_soaring/R_files/em_fl_dt_recurse_70ind.RData") #emig_fledg_dates
 
-# STEP 1: decide on the threshhold of the number of bursts per day -------------------------------------------------------------
+#calculate ratios for each burst, instead of each day
+
+ratios <- lapply(flight, function (x){
+  
+  #calculate week and day since emigration
+  ind_dates <- emig_fledg_dates %>% 
+    filter(individual.local.identifier == x$local_identifier[1])
+  
+  x <- x %>% 
+    mutate(days_since_emig = difftime(timestamp,ind_dates$emigration_dt, units = c("days")),
+           weeks_since_emig = difftime(timestamp,ind_dates$emigration_dt, units = c("weeks")),
+           days_since_fled = difftime(timestamp,ind_dates$fledging_dt, units = c("days")),
+           weeks_since_fled = difftime(timestamp,ind_dates$fledging_dt, units = c("weeks"))) %>% 
+    mutate_at(vars(matches("_since_")), funs(ceiling(as.numeric(.))))
+  
+  #estimate n of unique days and n of bursts per day
+  burst_IDs <- x %>% 
+    group_by(days_since_emig) %>% 
+    summarise(n_bursts = n_distinct(burstID)) %>% 
+    filter(n_bursts >= thr)
+  
+  #filter the data
+  x_subset <- x %>% 
+    filter(days_since_emig %in% burst_IDs$days_since_emig)
+  
+  x_subset
+  
+}) %>% 
+  reduce(rbind)
+
+
+
+
+########################## daily stuff. no clear pattern. very slight increase in the linear/circling trend ###################
+# STEP 1: decide on the threshold of the number of bursts per day -------------------------------------------------------------
 
 #If below the threshold, don't include the day.
 n_ls <- lapply(flight, function (x){
@@ -32,12 +66,78 @@ n_ls <- lapply(flight, function (x){
            weeks_since_fled = difftime(timestamp,ind_dates$fledging_dt, units = c("weeks"))) %>% 
     mutate_at(vars(matches("_since_")), funs(ceiling(as.numeric(.))))
   
-  #estimate n of unique days and n of bursts per day
-  n_bursts <- x %>% 
-    group_by(days_since_emig) %>% 
-    summarise(n_bursts = n_distinct(burstID))
-    
-  n_bursts
+  #calculate the ratio of each mode of flight to total flight time (as number of rows)
+  ratios <- x %>% 
+    group_by(local_identifier, weeks_since_emig, days_since_emig, burstID) %>% 
+    mutate(total_flight_in_burst = n()) %>% 
+    group_by(local_identifier, weeks_since_emig, days_since_emig,burstID, thermalClust) %>% 
+    summarize(amount_in_burst = n(),
+              total_flight_in_burst = head(total_flight_in_burst,1)) %>% 
+    group_by(local_identifier, weeks_since_emig, days_since_emig, burstID) %>% 
+    complete(thermalClust, fill = list(amount_in_burst = 0)) %>% #add zero for instances where a factor level was not observed in a burst
+    mutate(ratio = amount_in_burst/total_flight_in_burst)
+  
+  #plot----
+  ggplot(data = ratios, aes(x = weeks_since_emig, y = ratio)) +
+    geom_jitter() +
+    geom_smooth(method = "lm") +
+    facet_wrap(~ thermalClust) +
+    theme_classic()
+  #--------
+ 
+  #calculate the ratio of thermal soaring to linear soaring (as number of rows)
+  soaring_ratio <- x %>% 
+    group_by(local_identifier, weeks_since_emig, days_since_emig, burstID, thermalClust) %>% #group by all these variables to make sure they are retained in the final data frame
+    summarize(n_rows = length(thermalClust)) %>% 
+    group_by(local_identifier, weeks_since_emig, days_since_emig, burstID) %>% 
+    complete(thermalClust, fill = list(n_rows = 0)) %>% #from library(tidyr). wherever one factor level had no rows, insert a zero
+    summarize(soaring_ratio = n_rows[thermalClust == "circular"]/ n_rows[thermalClust == "linear"])
+  
+  #plot----
+  ggplot(data = soaring_ratio, aes(x = weeks_since_emig, y = soaring_ratio)) +
+    geom_jitter() +
+    geom_smooth(method = "lm")
+  #--------
+  
+  
+})
+  
+  
+  
+  
+  
+  #------------------------------old stuff
+  #calculate total flight and amount of flight in each category as the number of rows 
+  flight_summary <- data %>% 
+    group_by(local_identifier, days_since_emig) %>% 
+    mutate(total_flight_in_day = n()) %>% 
+    group_by(local_identifier, days_since_emig, flight_type) %>% 
+    summarize(amount_in_day = n(),
+              total_flight_in_day = head(total_flight_in_day,1),
+              weeks_since_emig = head(weeks_since_emig,1))
+  
+  ratios <- flight_summary %>% 
+    mutate(general_behaviors = ifelse(flight_type %in% c("soar_circular", "circular_no_ID"), "soaring", flight_type)) %>% 
+    group_by(local_identifier, days_since_emig) %>%
+    mutate(ratio = amount_in_day/total_flight_in_day)
+  
+  #just look at thermal clust: circular and linear (omit other. which is probably gliding)
+  ratio_soaring <-  data %>% 
+    group_by(local_identifier, days_since_emig) %>% 
+    mutate(total_flight_in_day = n()) %>% 
+    filter(thermalClust %in% c("circular", "linear")) %>% #get rid of the OTHER category
+    group_by(local_identifier, days_since_emig, thermalClust) %>% 
+    summarize(amount_in_day = n(),
+              total_flight_in_day = head(total_flight_in_day,1),
+              weeks_since_emig = head(weeks_since_emig,1)) %>% 
+    arrange(thermalClust, .by_group = T) %>% 
+    summarize(ratio = tail(amount_in_day,1)/head(amount_in_day,1),
+              weeks_since_emig = head(weeks_since_emig,1)) #i.e. linear/circular
+  
+  
+  
+  
+  
 }) %>% 
   reduce(rbind)
 
@@ -122,3 +222,5 @@ ggplot(data = ratio_soaring, aes(x = weeks_since_emig, y = ratio)) +
   #ylim(0,20) +
   geom_jitter() +
   geom_smooth(method = "lm")
+
+# STEP 2b: if only looking at ratio of linear/circling, no need to filter for days with certain n of bursts. OR try to clac ratios per burst instead of per day.....
--- WP3_Soaring_Ontogeny/MS2_flight_landscape/cluster_prep/alpine_pred_slrm.R
+++ WP3_Soaring_Ontogeny/MS2_flight_landscape/cluster_prep/alpine_pred_slrm.R
@@ -13,37 +13,37 @@ inla.setOption(pardiso.license = "pardiso.lic")
 
 #STEP 1: open data and assign variables -----------------------------------------------------------------
 
-weeks <- c(1, 2, 3, 4, 24, 48, 104) 
+weeks <- c(1, 2, 3, 4, 24, 48, 104)
 
-alps_data <- readRDS("alps_alt_50_20_min_48_ind_wmissing_Jun_temp.rds") %>% 
-  as.data.frame() 
+alps_data <- readRDS("alps_alt_50_20_min_48_ind_wmissing_Jun_temp.rds") %>%
+  as.data.frame()
 
 #try with a sample to test the cluster
-#alps_data <- alps_data %>% 
-#  group_by(used) %>% 
+#alps_data <- alps_data %>%
+#  group_by(used) %>%
 #  slice(1:1000)
 
 weeks_z_info <- readRDS("weeks_since_z_info.rds")
 
 #define variables and formula
-formulaM <- used ~ -1 + 
+formulaM <- used ~ -1 +
   dem_100_z * weeks_since_emig_n_z +
-  TRI_100_z * weeks_since_emig_n_z + 
-  f(stratum, model = "iid", 
+  TRI_100_z * weeks_since_emig_n_z +
+  f(stratum, model = "iid",
     hyper = list(theta = list(initial = log(1e-6),fixed = T))) +
   f(ind1, dem_100_z, model = "iid",
-    hyper=list(theta=list(initial=log(1),fixed=F,prior="pc.prec",param=c(3,0.05)))) + 
+    hyper=list(theta=list(initial=log(1),fixed=F,prior="pc.prec",param=c(3,0.05)))) +
   f(ind2, TRI_100_z,  model = "iid",
     hyper=list(theta=list(initial=log(1),fixed=F,prior="pc.prec",param=c(3,0.05))))
 
 mean.beta <- 0
-prec.beta <- 1e-4 
+prec.beta <- 1e-4
 
 #STEP 2: build one model per week ------------------------------------------------------------------------
 
 results_list <- list()
 
-for(x in weeks){ 
+for(x in weeks){
   #create the new data
   new_data <- alps_data %>%
     mutate(weeks_since_emig_n = replace_na(weeks_since_emig_n, x)) %>%
@@ -52,47 +52,47 @@ for(x in weeks){
   
   #run model
   
-  M_pred <- inla(formulaM, family = "Poisson", 
+  M_pred <- inla(formulaM, family = "Poisson",
                  control.fixed = list(
                    mean = mean.beta,
                    prec = list(default = prec.beta)),
-                 data = new_data, 
-                 num.threads = 20,
+                 data = new_data,
+                 #num.threads = 20, # be careful of memory. it was on 20 and 40 before. the job finished, but there was a memory error in the error log.
                  control.predictor = list(compute = TRUE), #this means that NA values will be predicted.
                  control.compute = list(openmp.strategy = "huge", config = TRUE, cpo = F), #deactivate cpo to save computing power
                  inla.mode="experimental")
-                 
-                 # posterior means of coefficients
-                 graph <- as.data.frame(summary(M_pred)$fixed)
-                 colnames(graph)[which(colnames(graph)%in%c("0.025quant","0.975quant"))]<-c("Lower","Upper")
-                 colnames(graph)[which(colnames(graph)%in%c("0.05quant","0.95quant"))]<-c("Lower","Upper")
-                 colnames(graph)[which(colnames(graph)%in%c("mean"))]<-c("Estimate")
-                 
-                 graph <- graph %>% 
-                   mutate(Factor = rownames(graph),
-                          week = x)
-
-                 
-                 #extract info to make prediction plots--------------------------------------------------------------------------
-                 used_na <- which(is.na(new_data$used))
-                 
-                 #extract information for rows that had NAs as response variables
-                 preds <- data.frame(location.long = new_data[is.na(new_data$used) ,"location.long"],
-                                     location.lat = new_data[is.na(new_data$used) ,"location.lat"],
-                                     dem_100_z = new_data[is.na(new_data$used) ,"dem_100_z"],
-                                     TRI_100_z = new_data[is.na(new_data$used) ,"TRI_100_z"],
-                                     weeks_since_emig_n_z = new_data[is.na(new_data$used) ,"weeks_since_emig_n_z"],
-                                     preds = M_pred$summary.fitted.values[used_na,"mean"], 
-                                     preds_sd = M_pred$summary.fitted.values[used_na,"sd"]) %>%  
-                   mutate(prob_pres = exp(preds)/(1+exp(preds)),#this should be between 0-1
-                          week = x) 
-                 
-                 week_results <- list(graph, preds)
-                 
-                 results_list <- append(results_list, week_results)
-                 
-                 rm(M_pred,preds, graph, new_data)
-                 gc(gc())
+  
+  # posterior means of coefficients
+  graph <- as.data.frame(summary(M_pred)$fixed)
+  colnames(graph)[which(colnames(graph)%in%c("0.025quant","0.975quant"))]<-c("Lower","Upper")
+  colnames(graph)[which(colnames(graph)%in%c("0.05quant","0.95quant"))]<-c("Lower","Upper")
+  colnames(graph)[which(colnames(graph)%in%c("mean"))]<-c("Estimate")
+  
+  graph <- graph %>%
+    mutate(Factor = rownames(graph),
+           week = x)
+  
+  
+  #extract info to make prediction plots--------------------------------------------------------------------------
+  used_na <- which(is.na(new_data$used))
+  
+  #extract information for rows that had NAs as response variables
+  preds <- data.frame(location.long = new_data[is.na(new_data$used) ,"location.long"],
+                      location.lat = new_data[is.na(new_data$used) ,"location.lat"],
+                      dem_100_z = new_data[is.na(new_data$used) ,"dem_100_z"],
+                      TRI_100_z = new_data[is.na(new_data$used) ,"TRI_100_z"],
+                      weeks_since_emig_n_z = new_data[is.na(new_data$used) ,"weeks_since_emig_n_z"],
+                      preds = M_pred$summary.fitted.values[used_na,"mean"],
+                      preds_sd = M_pred$summary.fitted.values[used_na,"sd"]) %>%
+    mutate(prob_pres = exp(preds)/(1+exp(preds)),#this should be between 0-1
+           week = x)
+  
+  week_results <- list(graph, preds)
+  
+  results_list <- append(results_list, week_results)
+  
+  rm(M_pred,preds, graph, new_data)
+  gc(gc())
 }
-                   
+
 saveRDS(results_list, file = "results_list.rds")
